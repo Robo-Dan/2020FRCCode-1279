53e0218200e6aedf89fc47a090fbdc3a2b534365
Robo-Dan <danjr805@gmail.com>
2020-01-08 20:40:03 -0500

    Worked on Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,21 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants
 {
@@ -32,12 +40,27 @@ public final class Constants
         public static final int TALON_TEN = 10;
     }
 
-    public static final class DriverJoystick
+    public static final class JoystickButtonNumbers
     {
         public static final int BUTTON_ONE = 1;
         public static final int BUTTON_TWO = 2;
         public static final int BUTTON_THREE = 3;
         public static final int BUTTON_FOUR = 4;
         public static final int BUTTON_FIVE = 5;
+    }
+
+    public static final class DriverJoystick
+    {
+        public static Joystick driverStick = new Joystick(0);
+    }
+
+    public static final class TalonNames
+    {
+        public static WPI_TalonSRX frontLeft = new WPI_TalonSRX(Constants.TalonIDs.TALON_ONE);
+        public static WPI_TalonSRX rearLeft = new WPI_TalonSRX(Constants.TalonIDs.TALON_TWO);
+        public static SpeedControllerGroup m_left = new SpeedControllerGroup(frontLeft, rearLeft);
+        public static WPI_TalonSRX frontRight = new WPI_TalonSRX(Constants.TalonIDs.TALON_THREE);
+        public static WPI_TalonSRX rearRight = new WPI_TalonSRX(Constants.TalonIDs.TALON_FOUR);
+        public static SpeedControllerGroup m_right = new SpeedControllerGroup(frontRight, rearRight);
     }
 }
