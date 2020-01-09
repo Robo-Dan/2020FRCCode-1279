1943851211701d4827c7b6f5d05feeede8cf193b
Robo-Dan <danjr805@gmail.com>
2020-01-09 18:23:35 -0500

    Worked on the Robot Container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -24,9 +25,9 @@ public class RobotContainer
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final DriveTrain m_robotDrive = new DriveTrain();
 
-
-
+  XboxController m_driverController = new XboxController(0);
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
